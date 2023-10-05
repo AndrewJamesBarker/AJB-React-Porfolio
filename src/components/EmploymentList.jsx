c62fad852a0413ebc3b0fcea30327cc0e585dd34
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-10-04 21:26:21 -0400

    one more try date ordering.

--- src/components/EmploymentList.jsx
+++ src/components/EmploymentList.jsx
@@ -1,39 +1,55 @@
-import {useState, useEffect} from "react";
-import { parse, compareDesc } from 'date-fns';
+import { useState, useEffect } from "react";
+import { parse, format, compareDesc } from "date-fns";
 import SocialBar from "./SocialBar";
 
-export default function  EmploymentList() {
+export default function EmploymentList() {
   const [employment, setEmployment] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getEmployment = async () => {
-      fetch('https://cms.barksbytesdev.com/api/employment')
-      .then(res => res.json())
-      .then(data => setEmployment(data));
+      try {
+        const res = await fetch('https://cms.barksbytesdev.com/api/employment');
+        if (!res.ok) {
+          throw Error('Could not fetch the data');
+        }
+        const data = await res.json();
+        const sortedData = data.sort((a, b) => {
+          const dateA = parse(a.started_at, "dd/MM/yyyy h:mm aa", new Date());
+          const dateB = parse(b.started_at, "dd/MM/yyyy h:mm aa", new Date());
+          return compareDesc(dateA, dateB);
+        });
+        setEmployment(sortedData);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     }
-   getEmployment();
+    getEmployment();
   }, []);
-
-// employment section of site //
 
   return (
     <div>
       <SocialBar />
-       <main className="widthControl" id="mainSidePages">
+      <main className="widthControl" id="mainSidePages">
         <div className="basic-container">
-        {employment.map(item => (
-          <div className="basic-item" key={item.id}>
-            <h2>{item.title}</h2>
-            <p>From: {item.started_at} To: {item.ended_at}</p>
-            <p><a href={item.url}>{item.employer}</a></p>
-            <p>{item.content}</p>
-            
-          </div>
-        ))}
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+          {employment.map(item => (
+            <div className="basic-item" key={item.id}>
+              <h2>{item.title}</h2>
+              <p>
+                From: {format(parse(item.started_at, "dd/MM/yyyy h:mm aa", new Date()), 'MMMM yyyy')}
+                To: {item.ended_at ? format(parse(item.ended_at, "dd/MM/yyyy h:mm aa", new Date()), 'MMMM yyyy') : 'Present'}
+              </p>
+              <p><a href={item.url}>{item.employer}</a></p>
+              <p>{item.content}</p>
+            </div>
+          ))}
         </div>
-       </main>
-
+      </main>
     </div>
   );
-};
-
+}

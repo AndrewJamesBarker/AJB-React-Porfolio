d5e7bf4b5e0fb8ec5bfb815aac5e41affdea3d93
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-10-12 15:56:54 -0400

    landing page CTA update.

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -31,7 +31,7 @@ return (
         <div id="heroText">
             <p>Hello there! I'm</p><h2>Andrew Barker.</h2>
             <p>I’m a Junior Full-Stack Web Developer. My portfolio is growing, and I’m always looking for new things to build, and learn.</p>
-            <p>I love clean, intuitive designs and strive to create a seamless interactive experience.</p>
+            <p>I love clean, intuitive designs and strive to create seamless interactive experiences.</p>
         </div>
 
         {/* <!-- Portfolio Items --> */}

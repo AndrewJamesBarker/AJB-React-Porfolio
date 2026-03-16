7bfa886c5ae10d585101ee527a343bbe0a2a49d0
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2026-03-15 22:39:11 -0400

    update hero text

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -122,10 +122,10 @@ export default function MainBody() {
           <p>Hello there! I'm</p>
           <h2>Andrew Barker.</h2>
           <p>
-            I’m a Full-Stack Developer focused on scalable web applications and CMS platforms, helping people bring their ideas to life on the web.
+            I’m a Full-Stack Developer focused on scalable web applications and CMS platforms.
           </p>
           <p>
-            I focus on creating digital products that are simple, reliable, and effective. I am also AWS Developer certified, with experience building and deploying modern cloud-based applications.
+            I am also AWS Developer certified, with experience building and deploying modern cloud-based applications.
           </p>
         </div>
 

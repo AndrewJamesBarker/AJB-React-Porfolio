e398fbc2ef6d5088e1fc36d3c05374fb100ece6c
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2026-03-15 20:43:43 -0400

    edit about section more

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,32 +1,43 @@
 import SocialBar from "./SocialBar";
 
 export default function AboutPage() {
-	return (
-		<div>
-			<SocialBar />
-			<main id="mainAbout" className="widthControl">
-				<div className="basic-container">
-					<div className="basic-item">
-						<h1>About</h1>
-						<p>
-							Andrew Barker is a Toronto-based full-stack web developer, guitarist, music educator, and all-around creative tinkerer.
-						</p>
-						<p>
-							He holds a Bachelor’s degree in Jazz Performance with Honours and a Graduate Certificate in Web Development with Honours, both from Humber College. Andrew enjoys connecting ideas and helping others turn their visions into reality online, whether that means creating a website, developing an application, or organizing/migrating data.
-						</p>
-						<p>
-							He’s good at explaining technical concepts in plain English and loves helping people build meaningful things.
-						</p>
+  return (
+    <div>
+      <SocialBar />
+      <main id="mainAbout" className="widthControl">
+        <div className="basic-container">
+          <div className="basic-item">
+            <h1>About</h1>
+            <p>
+              Andrew Barker is a full-stack web developer based in Toronto, a
+              guitarist, music educator, and creative tinkerer.{" "}
+            </p>
+            <p>
+              He earned a Bachelor’s degree in Jazz Performance with Honours and
+              a Graduate Certificate in Web Development with Honours from Humber
+              College.
+            </p>
+            <p>
+              Andrew enjoys linking ideas and helping others turn their visions
+              into online realities, whether by creating websites, developing
+              applications, or organizing and migrating data. He is skilled at
+              demystifying technical concepts in simple language and finds joy
+              in helping people build meaningful projects.
+            </p>
 
-						<h2>Education</h2>
-						<ul>
-							<li>Bachelor’s degree in Jazz Performance (Honours) — Humber College</li>
-							<li>Graduate Certificate in Web Development (Honours) — Humber College</li>
-						</ul>
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+            <h2>Education</h2>
+            <ul>
+              <li>
+                Bachelor’s degree in Jazz Performance (Honours) — Humber College
+              </li>
+              <li>
+                Graduate Certificate in Web Development (Honours) — Humber
+                College
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
-

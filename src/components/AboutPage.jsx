0c3a03b328e0db7d84ce26b8960121c2548ab026
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2026-03-15 22:43:02 -0400

    update about text to first person

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -9,20 +9,20 @@ export default function AboutPage() {
           <div className="basic-item">
             <h1>About</h1>
             <p>
-              Andrew Barker is a full-stack web developer based in Toronto, a
-              guitarist, music educator, and creative tinkerer.{" "}
+              I am a Toronto-based full-stack web developer, guitarist, music
+              educator, and creative tinkerer.
             </p>
             <p>
-              He earned a Bachelor’s degree in Jazz Performance with Honours and
+              I earned a Bachelor’s degree in Jazz Performance with Honours and
               a Graduate Certificate in Web Development with Honours from Humber
               College.
             </p>
             <p>
-              Andrew enjoys linking ideas and helping others turn their visions
-              into online realities, whether by creating websites, developing
-              applications, or organizing and migrating data. He is skilled at
-              demystifying technical concepts in simple language and finds joy
-              in helping people build meaningful projects.
+              I enjoy linking ideas and helping others turn their visions into
+              online realities, whether by creating websites, developing
+              applications, or organizing and migrating data. I am skilled at
+              demystifying technical concepts in simple language and find joy in
+              helping people build meaningful projects.
             </p>
 
             <h2>Education</h2>

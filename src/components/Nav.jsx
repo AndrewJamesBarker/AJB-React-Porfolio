2ec37db68d64d0d52ff356268127af54a9392ebb
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2026-03-15 20:08:56 -0400

    comment out employment link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,7 @@ export default function Nav() {
     {/* <button className="menu-toggle"><i className="fa-solid fa-bars"></i></button> */}
     <ul className="menu">
       <Link to="/" className="nav-link">PROJECTS</Link>
-      <span className="nav-link disabled" aria-disabled="true">EMPLOYMENT</span>
+      {/* <span className="nav-link disabled" aria-disabled="true">EMPLOYMENT</span> */}
       <Link to="/education" >EDUCATION</Link>
     </ul>
   </nav>
